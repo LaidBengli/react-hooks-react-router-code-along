5969cf400db62533ac3cf6b1c923062985d4030f
LaidBenglia <73167661+LaidBengli@users.noreply.github.com>
2023-09-11 03:48:28 -0700

    Done

---
+++ src/components/Login.js
@@ -0,0 +1,17 @@
+export default function Login() {
+    return (
+      <div>
+        <h1>Login</h1>
+        <form>
+          <div>
+            <input type="text" name="username" placeholder="Username" />
+          </div>
+          <div>
+            <input type="password" name="password" placeholder="Password" />
+          </div>
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    );
+  }
+  
